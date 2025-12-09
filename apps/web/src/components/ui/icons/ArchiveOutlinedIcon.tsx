774ff48ee045632c778e7dc32f13b52d920f86ec
nookworth <nooksack@gmail.com>
2025-12-09 11:03:16 -0700

    feat(icons): tbb-75 add and refactor icons
    through 'ArchiveSolidIcon'

---
+++ apps/web/src/components/ui/icons/ArchiveOutlinedIcon.tsx
@@ -0,0 +1,8 @@
+export const ArchiveOutlinedIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M20 9H4v10a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1zm-6 2a1 1 0 1 1 0 2h-4a1 1 0 1 1 0-2zM2 4v3h20V4zm22 3a2 2 0 0 1-2 2v10a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V9a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h20a2 2 0 0 1 2 2z"
+      fill="#1e1e1e"
+    />
+  </svg>
+)
